e68b645459922be221fb429d80b9508ee7113225
Partast1 <43130360+Partast1@users.noreply.github.com>
2020-03-19 18:19:30 +0100

    a

--- Card.js
+++ Card.js
@@ -8,18 +8,7 @@ class Card {
     }
 
 }
-let card = document.getElementsByClassName("Card");
-let cards = [...card];
-// loop to add event listeners to each card
-for (var i = 0; i < cards.length; i++){
-    cards[i].addEventListener("click", displayCard);
-};
 
-// var displayCard = function (){
-//     this.classList.toggle("open");
-//     this.classList.toggle("show");
-//     this.classList.toggle("disabled");
-// };
 function Shuffle() {
     var currentIndex = cards.length, temporaryValue, randomIndex;
 
@@ -32,29 +21,8 @@ function Shuffle() {
     }
     return cards;
 }
-var GenerateImages = function () {
-    var imgArray = new Array();
 
-    imgArray[0] = new Image();
-    imgArray[0].src = 'Imgs/Cow.jpg';
-
-    imgArray[1] = new Image();
-    imgArray[1].src = 'Imgs/Owl.jpg';
-
-    imgArray[2] = new Image();
-    imgArray[2].src = 'Imgs/Panda.jpg';
-};
-// var GenerateMap2 = function () {
-//     var canvas = document.getElementById("card2");
-//     var ctx = canvas.getContext("2d");
-//     var img = new Image();
-//     img.src = "Imgs/Cow.jpg";
-//
-//     img.onload = () => {
-//         ctx.drawImage(img,0,0, 200, 200)
-//     }
-// };
-function generateCards(game) {
+function generateCards() {
     let ImageNames = [
         "Owl",
         "Cow",
@@ -71,22 +39,30 @@ function generateCards(game) {
     ];
     let cardsWithImg = [];
     // Create a pair for each different name
-    for (var i = 0; i < ImageNames.length && i < cards.length / 2; i++) {
-        var cardFace1 = new Image();
-        var cardFace2 = new Image();
-        cardFace1.src = "./" + ImageNames[i] + ".jpg";
-        cardFace2.src = "./" + ImageNames[i] + ".jpg";
-        cardFace1.id = i;
-        cardFace2.id = i;
-        cardFace1.classList.add("card-value");
-        cardFace2.classList.add("card-value");
-        cardsWithImg.push(cardFace1);
-        cardsWithImg.push(cardFace2);
+// && i < cards.length / 2;
+    for (let i = 0; i < ImageNames.length ; i++) {
+        let cardFront1 = new Image();
+        let cardFront2 = new Image();
+        cardFront1.addEventListener("click", displayCard);
+        cardFront2.addEventListener("click", displayCard);
+        cardFront1.src = "Imgs/" + ImageNames[i] + ".jpg";
+        cardFront2.src = "Imgs/" + ImageNames[i] + ".jpg";
+        cardFront1.id = i;
+        cardFront2.id = i;
+        cardFront1.classList.add("card-value");
+        cardFront2.classList.add("card-value");
+        cardsWithImg.push(cardFront1);
+        cardsWithImg.push(cardFront2);
+    }
+    for (let i = 0; i < cardsWithImg.length; i++){
+        let canvas = document.getElementById("card" + i);
+        let ctx = canvas.getContext("2d");
+        let img = cardsWithImg[i];
+
+        img.onload = () => {
+            ctx.drawImage(img,0,0)
+        }
+
     }
 
-    // scrambleArray(cardFaces);
-    // for (var i = 0; i < game.cardArray.length; i++) {
-    //     game.cardArray[i].id = cardFaces[i].id;
-    //     getChildByClassName(game.cardArray[i], "card-front").appendChild(cardFaces[i]);
-    // }
 }
